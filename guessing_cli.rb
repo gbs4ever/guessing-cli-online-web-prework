3b67e2ffb1ef201f2a8f9a5b56c31e88514e29c1
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 20:36:32 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
 user_input = ""
   while user_input
       puts "Guess a number between 1 and 6."
-      pcans1 = rand(1..6).to_s
+      pcans = rand(1..6).to_s
       user_input= gets.chomp
       pcans=pcans1
     if  user_input == "exit"
